aea27eefcf16692a334457a68217149c3e03d575
Alif Aiman <englios656@gmail.com>
2024-01-25 13:58:29 +0800

    Added more commands and fix daily job issue

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-import bot.bot as bot
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from bot import commands,daily,responses
+from main_utils import vars
+from telegram.ext import Application, CommandHandler, MessageHandler,Updater,filters
 def main():
     
     print('Starting Bot')
-    app = Application.builder().token(bot.TOKEN).build()
+    app = Application.builder().token(vars.TELEGRAM_BOT_KEY).build()
     
     #Commands
-    app.add_handler(CommandHandler('start',bot.start_command))
-    app.add_handler(CommandHandler('help',bot.help_command))
-    app.add_handler(CommandHandler('custom',bot.custom_command))
-    app.add_handler(CommandHandler('daily',bot.send_daily))
+    app.add_handler(CommandHandler('help',commands.help_command))
+    app.add_handler(CommandHandler('start',commands.start_command))
+    app.add_handler(CommandHandler("info", commands.info_command))
+    
+    # Daily Commands
+    app.add_handler(CommandHandler('daily',daily.send_daily))
+    app.add_handler(CommandHandler("schedule", daily.schedule_daily_job))
+    app.add_handler(CommandHandler("schedule_stop", daily.stop_daily_job))
     
     #Messages
-    app.add_handler(MessageHandler(filters.TEXT,bot.handle_message))
+    app.add_handler(MessageHandler(filters.TEXT,responses.handle_message))
     #Errors
-    app.add_error_handler(bot.error)
-    
+    app.add_error_handler(responses.error)
+
+
     #Polling
     print("Polling....")
     
@@ -23,7 +29,7 @@ def main():
         app.run_polling(poll_interval=3)
     except KeyboardInterrupt:
         print("Stopping bot...")
-        bot.stop_bot()
+        daily.stop_bot(app)
         print("All tasks cancelled.")
     
 
