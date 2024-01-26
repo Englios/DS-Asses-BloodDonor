e402090d8fd30cf12922848aff7638fa27d0549d
Alif Aiman <englios656@gmail.com>
2024-01-26 13:07:02 +0800

    Add dpi parameter to save_fig function

--- main_utils/save_fig.py
+++ main_utils/save_fig.py
@@ -3,12 +3,22 @@ import matplotlib.pyplot as plt
 
 IMAGE_DIR = './images'
 
-def save_fig(fname: str):
+def save_fig(fname: str, dpi: int = 600):
+    """
+    Save the current figure to a file.
+
+    Parameters:
+    - fname (str): The filename to save the figure to.
+    - dpi (int): The resolution of the saved figure in dots per inch. Default is 300.
+
+    Returns:
+    None
+    """
     try:
-        plt.savefig(os.path.join(IMAGE_DIR,fname))
+        plt.savefig(os.path.join(IMAGE_DIR, fname), dpi=dpi)
     except:
-        os.remove(os.path.join(IMAGE_DIR,fname))
+        os.remove(os.path.join(IMAGE_DIR, fname))
     else:
-        plt.savefig(os.path.join(IMAGE_DIR,fname))
+        plt.savefig(os.path.join(IMAGE_DIR, fname), dpi=dpi)
     finally:
         plt.close()
