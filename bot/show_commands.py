5cd2ba79a4b17ed6690439d75b24480bd29e5004
Alif Aiman <englios656@gmail.com>
2024-01-27 10:34:05 +0800

    Added image captions and updated image paths

--- bot/show_commands.py
+++ bot/show_commands.py
@@ -8,10 +8,11 @@ async def show_malaysia_command(update:Update,context:ContextTypes.DEFAULT_TYPE)
     message = f"""
     Hello here is the latest statistics for Malaysia
     """
+    #Show Trend
     await context.bot.send_photo(chat_id=chat_id,
                                  photo=open('./images/trend_donations_malaysia.jpg', 'rb'),
                                  caption = f'As we can see,Malaysia is on the rise with Blood Donations,which means more and more people are donating blood.\nThe data for 2024 is still in collection,but it will most likely rise from previous years')
-    
+    #Show 7 Day Avg
     await context.bot.send_photo(chat_id=chat_id,
                                  photo=open('./images/trend_7_day_avg_malaysia_2023.jpg', 'rb'),
                                  caption = f'By Taking a closer look over 2023 - 2024.\nIt can be seen that our daily Blood Donations are stable,with daily an average daily donation count.\nThe valley formed around 2023-4 to 2023-5 is most likely due to fasting month,where less blood donations are peformed by the Muslim population.')
@@ -20,8 +21,6 @@ async def show_malaysia_command(update:Update,context:ContextTypes.DEFAULT_TYPE)
                                 photo=open('./images/trend_7_day_avg_malaysia_2021.jpg', 'rb'),
                                 caption = f'This is further reinforced by looking at the data from 2021 - 2022,where a valley is formed early in 2021-5,which for that year,Ramadhan falls on May.')
     
-    await context.bot.send_photo(chat_id=chat_id,
-                                 photo=open("./images/"))
 
 async def show_states_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
@@ -47,6 +46,30 @@ async def show_states_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
     
 async def show_retention_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
+    
     await context.bot.send_photo(chat_id=chat_id,
-                                photo=open('./images/donation_trend_all_years.jpg', 'rb'),
-                                caption = f"So by plotting the time series data by year for all the states excluding KL(because it's an outlier),we can see a much clearer trend of donation for all the states within Malaysia.There's no data for Labuan,Putrajaya and Perli,which explains the absence of these states")
+                                photo=open('./images/trend_donor_types_malaysia.jpg', 'rb'),
+                                caption = f"Donors in Malaysia can be divided into two types,New and Returning Donors.\nReturning Donors can further be broken down into Regular Donor(those who donate regularly within 2 years) and Lapsed Donor(those who donate again after 2 years).\nThe plot shows the trend of donors in Malaysia,where we see more returning donors and new donors.\nThe dip in regular donors after 2020 can be attributed to COVID19,which made donating difficult,but is on the rise again.")
+    
+    await context.bot.send_photo(chat_id=chat_id,
+                                photo=open('./images/trend_donor_types_malaysia.jpg', 'rb'),
+                                caption = f"Thus a plot of retention rates can be used.\nThe retention rate is calculated by using (type of returning donors/total_donors)*100.\nHere we see a trend of more regular donors returning in the year 2023.")
+
+async def show_new_donors_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
+    chat_id = update.message.chat_id
+    
+    await context.bot.send_photo(chat_id=chat_id,
+                                photo=open('./images/trend_donor_type_malaysia.jpg', 'rb'),
+                                caption = f"Malaysia is also receiving new donors amongst all blood donors,and the number is steadily rising again after COVID19")
+    
+    await context.bot.send_photo(chat_id=chat_id,
+                                photo=open('./images/trend_new_donors_malaysia.jpg', 'rb'),
+                                caption = f"A clearer picture can be seen in this plot,where we see a decline after 2020,which was due to COVID19.\n Another things to attribute towards the decline is the previous new donors being classified as returning donors,which shows that Malaysia is retaining donors well")
+    
+    await context.bot.send_photo(chat_id=chat_id,
+                                photo=open('./images/trend_new_donors_age_groups.jpg', 'rb'),
+                                caption = f"From 2023 - 2024, the younger generation (17-24) makes up the brunt of the donation force")
+    
+
+    
+    
