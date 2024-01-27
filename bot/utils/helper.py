f28b9d6957ca20976cef4d06942dc506a3c567a9
Alif Aiman <englios656@gmail.com>
2024-01-28 01:05:57 +0800

    Add NLTK word tokenization and lemmatization functions

--- bot/utils/helper.py
+++ bot/utils/helper.py
@@ -1,4 +1,9 @@
 import os
+import nltk
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 def get_message_string(file_path):
     with open(file_path,'r',encoding='utf-8') as f:
@@ -10,3 +15,15 @@ def daily(context, chat_id):
     photo_path = os.path.join(os.getcwd(), "images", "test1.png")
     daily_message = os.path.join(os.getcwd(),"daily_texts","daily_message.txt")
     context.bot.send_message(chat_id=chat_id, text=get_message_string(daily_message))
+    
+def tokenize_words(sentence:str):
+    return word_tokenize(sentence)
+    
+def get_lemmatized_words(sentence:str):
+    tokens = word_tokenize(sentence)
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [word for word in tokens if word.casefold() not in stop_words]
+    
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word,pos='v') for word in filtered_words]    
+    return lemmatized_words 
