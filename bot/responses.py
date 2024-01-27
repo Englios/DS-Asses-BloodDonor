6bdb20e70355065505d75583920a97d895e9604a
Alif Aiman <englios656@gmail.com>
2024-01-28 02:27:13 +0800

    Refactor response handling and add show parser

--- bot/responses.py
+++ bot/responses.py
@@ -5,30 +5,48 @@ from .show_commands import *
 from main_utils import vars
 from .utils import helper
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes,CommandHandler
 
 
 BOT_USERNAME = vars.BOT_USERNAME
 
 # Responses
-def handle_response(text:str,update,context) -> str:
+def handle_response(text:str) -> str:
     processed_text = helper.get_lemmatized_words(text.lower())
 
     if all(keyword in processed_text for keyword in ['malaysia','trend','blood','donations']):
-        return show_malaysia_command()
+        return "Here's the trend in Malaysia"
         
     elif all(keyword in processed_text for keyword in ['states','trend','blood','donations']):
-        return show_states_command()
+        return "Here's the trend in the States"
         
     elif any(keyword in processed_text for keyword in ['retain', 'return', 'regular','lapse']):
-        return show_retention_command()
+        return "Here's how many donors are returning!"
     
     elif any(keyword in processed_text for keyword in ['new','blood','donations','donor']):
-        return show_new_donors_command()
+        return "Here's the trend of New Blood Donors"
     
     else:
         return 'I am not programmed to understand that yet'
 
+#Show parser
+async def show_parser(response:str,update,context):
+    if response == "Here's the trend in Malaysia":
+        await show_malaysia_command(update, context)
+        
+    elif response == "Here's the trend in the States":
+       await show_states_command(update, context)
+        
+    elif response == "Here's how many donors are returning!":
+        await show_retention_command(update, context)
+    
+    elif response == "Here's the trend of New Blood Donors":
+        await show_new_donors_command(update, context)
+    
+    else:
+        pass
+
+#Handle Message Function
 async def handle_message(update:Update,context:ContextTypes.DEFAULT_TYPE):
     message_type: str = update.message.chat.type
     text:str = update.message.text  
@@ -38,7 +56,6 @@ async def handle_message(update:Update,context:ContextTypes.DEFAULT_TYPE):
         if BOT_USERNAME in text:
             new_text: str = text.replace(vars.BOT_USERNAME, '').strip()
             response:str = handle_response(new_text)
-            
         else:
             return 
     else:
@@ -46,7 +63,9 @@ async def handle_message(update:Update,context:ContextTypes.DEFAULT_TYPE):
         
     print('Bot:',response)
     await update.message.reply_text(response)
-    
+    await show_parser(response,update,context)
+
+#Handle Errors
 def error(update:Update,context:ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
     
